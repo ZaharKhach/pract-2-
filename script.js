b78e0148143bb4f434e2b8891c209b1e3f9fc8be
Zahar <zahar@gmail.com>
2023-01-21 16:05:32 +0100

    third task

--- script.js
+++ script.js
@@ -33,5 +33,14 @@ first: for (let i = 0; i < 2; i++) {
 }
 
 console.log(personalMovieDB.movies);
+if (personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов");
+}else if (personalMovieDB.count>=10 && personalMovieDB.count <=30){
+    console.log("Вы классический зритель")
+}else if (personalMovieDB.count > 30){
+    console.log("Вы киноман")
+}else{
+    console.log("Произошла ошибка");
+}
 
 // console.log(movies[lastFilm]);
